fd0a7538f1ef6e71f0ebcc1948815df2daf9d52b
Anita Yeung <anita@slice.com>
2016-03-02 10:02:12 -0800

    working guessing game

--- guessingGame/guessingGame/ViewController.swift
+++ guessingGame/guessingGame/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var computerGuess = Int(arc4random_uniform(6))
+    var guesses = 0
+    
+    @IBOutlet weak var playerGuess: UITextField!
+    @IBAction func guess(sender: UIButton) {
+        performAction()
+    }
+    @IBOutlet weak var results: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        playerGuess.resignFirstResponder()
+        performAction()
+        return true
+    }
+    
+    func performAction() {
+        print("Computer guess is \(computerGuess)")
+        if Int(playerGuess.text!) == computerGuess {
+            results.text = "Good job!"
+            image.image = UIImage(named: "like.png")
+            computerGuess = Int(arc4random_uniform(6))
+            guesses = 0
+        } else {
+            results.text = "Nope. Try again"
+            image.image = UIImage(named: "dislike.png")
+            guesses++
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.playerGuess.delegate = self
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
