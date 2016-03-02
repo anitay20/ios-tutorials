65a1a2ed81a30cfacf755c2da6704e69fb2b4304
Anita Yeung <anita@slice.com>
2016-03-02 01:32:14 -0800

    Adding all the files

--- displayText/displayText/ViewController.swift
+++ displayText/displayText/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var displayText: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func submit(sender: UIButton) {
+        performAction()
+    }
+    @IBAction func reset(sender: UIButton) {
+        displayText.text = "Display Text"
+        textField.text = nil
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        performAction()
+        return true
+    }
+    
+    func performAction() {
+        displayText.text = textField.text
+        textField.text = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
