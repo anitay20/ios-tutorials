69e984d9d5ca2c331fd10761531aa8af30b6c7ff
Anita Yeung <anita@slice.com>
2016-03-03 21:35:58 -0800

    Adding all files.

--- isItPrime?/isItPrime?/ViewController.swift
+++ isItPrime?/isItPrime?/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBAction func submit(sender: UIButton) {
+//        number.resignFirstResponder()
+        performAction()
+    }
+    
+    func isPrime(num: Int) -> Bool {
+        var isPrime = true
+        if num > 2 {
+            for var i = 2; i < num; i++ {
+                if num % i == 0 {
+                    isPrime = false
+                }
+            }
+        }
+        return isPrime
+    }
+    
+    func textFieldShouldReturn(number: UITextField) -> Bool {
+        number.resignFirstResponder()
+        performAction()
+        return true
+    }
+    
+    func performAction() {
+        if isPrime(Int(number.text!)!) {
+            result.text = "\(number.text!) is a prime number"
+        } else {
+            result.text = "\(number.text!) is NOT a prime number"
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.number.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
