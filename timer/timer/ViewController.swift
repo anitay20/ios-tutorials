d2dc9428396fb29126f19a894156c2e45ccf2be2
Anita Yeung <anita@slice.com>
2016-03-18 15:17:36 -0700

    timer app is working

--- timer/timer/ViewController.swift
+++ timer/timer/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    var timer = NSTimer()
+    
+    @IBOutlet weak var time: UILabel!
+    
+    @IBAction func start(sender: UIBarButtonItem) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pause(sender: UIBarButtonItem) {
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func stop(sender: UIBarButtonItem) {
+        timer.invalidate()
+        reset()
+    }
+    
+    func reset() {
+        timer.invalidate()
+        count = 0
+        time.text = "\(count)"
+    }
+    
+    func result() {
+        count += 1
+        time.text = String(count)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
