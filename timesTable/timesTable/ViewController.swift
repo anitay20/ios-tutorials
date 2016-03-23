eda728bf53fb10154c921e976ac954ff6a1ec6ef
Anita Yeung <anita@slice.com>
2016-03-23 13:04:11 -0700

    More files for timesTable

--- timesTable/timesTable/ViewController.swift
+++ timesTable/timesTable/ViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    @IBAction func numberSlider(sender: UISlider) {
+        table.reloadData()
+        number.text = "\(Int(sliderValue.value))"
+    }
+
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var number: UILabel!
+
+    
+    func createArray(num: Int) -> [Int] {
+        var ints = [Int]()
+        
+        for i in 1...num {
+            ints.append(i)
+        }
+        return ints
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(sliderValue.value)
+        cell.textLabel?.text = String(timesTable  * (indexPath.row + 1))
+        return cell
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+
+        
     }
 
     override func didReceiveMemoryWarning() {
