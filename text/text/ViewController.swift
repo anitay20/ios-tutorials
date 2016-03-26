a298f9c62fb203846c900050958974d48b27ba69
Anita Yeung <anita@slice.com>
2016-03-25 18:10:11 -0700

    Same as display text.

--- text/text/ViewController.swift
+++ text/text/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
+
+    @IBOutlet weak var enteredText: UITextField!
+    @IBOutlet weak var showText: UILabel!
+    @IBAction func submitText(sender: UIButton!) {
+        showText.text = enteredText.text
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.enteredText.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
