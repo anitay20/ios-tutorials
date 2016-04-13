143ed52b96e084d17a79e59c02a65d73fadb09c0
Anita Yeung <anita@slice.com>
2016-04-12 17:57:55 -0700

    Kind of working tic tac toe.
    The index out of range is still crashing the app and when anything wins, it says it's a draw.

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,25 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var activePlayer = 1 // 1 = x, 2 = o
+    var activePlayer = 1 // 1 = O, 2 = X
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    let winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                         [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                         [0, 4, 8], [2, 4, 6]]
+    let winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                         [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                         [1, 5, 9], [3, 5, 7]]
     var gameActive = true
 
 
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var gameOver: UILabel!
+    @IBOutlet weak var playAgain: UIButton!
     @IBAction func playAgain(sender: UIButton) {
         reset()
+        print((gameState))
     }
-    @IBOutlet weak var playAgain: UIButton!
+
     
     @IBAction func buttonPressed(sender: UIButton) {
         
-        if (gameState[sender.tag] == 0 && gameActive == true) {
-            gameState[sender.tag] = activePlayer
+        if (gameState[sender.tag-1] == 0 && gameActive == true) {
+            gameState[sender.tag-1] = activePlayer
         
             if activePlayer == 1 {
                 sender.setImage(UIImage(named: "o"), forState: .Normal)
@@ -37,7 +39,7 @@ class ViewController: UIViewController {
                 sender.setImage(UIImage(named: "x"), forState: .Normal)
                 activePlayer = 1
             }
-        
+            print((gameState))
             for combo in winningCombos {
                 if (gameState[combo[0]] != 0 && gameState[combo[0]] == gameState[combo[1]] && gameState[combo[1]] == gameState[combo[2]]) {
                     
@@ -50,29 +52,43 @@ class ViewController: UIViewController {
                         print("X won!")
                         gameOver.text = "X won!"
                     }
-                    gameOver.hidden = false
-                    UIView.animateWithDuration(0.5, animations: { () -> Void in
-                        self.gameOver.center = CGPointMake(self.gameOver.center.x + 500, self.gameOver.center.y)
-                        self.playAgain.center = CGPointMake(self.playAgain.center.x + 500, self.playAgain.center.y)
-                        
-                    })
-                    
-                    
+                    endGame()
                 }
             }
+            if gameActive {
+                gameActive = false
+                for buttonState in gameState {
+                    if buttonState == 0 {
+                    gameActive = true
+                    }
+                }
+            }
+            if !gameActive {
+                gameOver.text = "It's a draw"
+                endGame()
+            }
         }
-        
-        
     }
     
+    
+    func endGame() {
+        gameActive = false
+        gameOver.hidden = false
+        playAgain.hidden = false
+        
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.gameOver.center = CGPointMake(self.gameOver.center.x + 500, self.gameOver.center.y)
+            self.playAgain.center = CGPointMake(self.playAgain.center.x + 500, self.playAgain.center.y)
+        })
+    }
+
     func reset() {
         activePlayer = 1
-        for state in gameState{
-            gameState[state] = 0
-        }
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        print((gameState))
         
         var buttonToClear: UIButton
-        for num in 0...9 {
+        for num in 1...9 {
             buttonToClear = self.view.viewWithTag(num) as! UIButton
             buttonToClear.setImage(nil, forState: .Normal)
         }
@@ -81,6 +97,8 @@ class ViewController: UIViewController {
         gameOver.center = CGPointMake(gameOver.center.x - 500, gameOver.center.y)
         playAgain.hidden = true
         playAgain.center = CGPointMake(playAgain.center.x - 500, playAgain.center.y)
+        
+        gameActive = true
     }
     
     override func viewDidLoad() {
@@ -89,7 +107,8 @@ class ViewController: UIViewController {
         
         gameOver.hidden = true
         gameOver.center = CGPointMake(gameOver.center.x - 500, gameOver.center.y)
-        
+        playAgain.hidden = true
+        playAgain.center = CGPointMake(playAgain.center.x - 500, playAgain.center.y)
     }
 
     override func didReceiveMemoryWarning() {
