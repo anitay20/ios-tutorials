707fd76eeb44a310eb003eb3ab1b06f0ae98f8e5
Anita Yeung <anita@slice.com>
2016-04-12 17:58:06 -0700

    playground file

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                     [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                     [1, 5, 9], [3, 5, 7]]
+
+for var combos in winningCombos {
+    for var i in combos {
+        print(i)
+    }
+}
+
+winningCombos
+
+for c in winningCombos {
+    print(c)
+    for i in c {
+        var hello = i
+        hello = hello - 1
+        print(hello)
+    }
+}
+
+winningCombos
+
+var combo = [1, 2, 3]
+for i in combo {
+    combo[i-1] -= 1
+}
+combo
