2d2d2c7e8d66d3b4ce65311e32fe1538711cacf7
anita <anita@anita.local>
2016-10-02 21:51:18 -0700

    bull’s eye

--- Bull's Eye/Bull's Eye/ViewController.swift
+++ Bull's Eye/Bull's Eye/ViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var targetNum: UILabel!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var round: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     var currentValue: Int = 0
     var targetValue: Int = 0
+    var points: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
-        targetValue = 1 + Int(arc4random_uniform(100))
-        
+        startNewRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -30,37 +34,48 @@ class ViewController: UIViewController {
     var roundNum = 0
 
     
-    @IBAction func tapMe(sender: AnyObject) {
-        let message = "The value of the slider is \(currentValue)"
-        let alert = UIAlertController(title: "Test", message: message, preferredStyle: .Alert)
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+    @IBAction func tapMe(_ sender: AnyObject) {
+        points = 100 - abs(targetValue - currentValue)
+        let message = "The value of the slider is \(currentValue).\nThe target was \(targetValue).\nYou got \(points) points!"
+        let alert = UIAlertController(title: "Test", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
         roundNum += 1
+        scoreNum += points
+        round.text = "\(roundNum)"
+//        slider.value = 50
+        targetValue = 1 + Int(arc4random_uniform(100))
+        targetNum.text = "\(targetValue)"
+        score.text = "\(scoreNum)"
+        
+    }
+
+    
+    @IBAction func startOver(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Start Over?", message: "Do you really want to start over? There's no turning back from this...", preferredStyle: .alert)
+        let OKaction = UIAlertAction(title: "OK", style: .default, handler: { (action) in self.startNewRound() })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alert.addAction(OKaction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
     
-    @IBOutlet weak var targetNum: UILabel!
-    @IBOutlet weak var score: UILabel!
-    @IBOutlet weak var round: UILabel!
-    @IBOutlet weak var slider: UISlider!
     
-    @IBAction func startOver(sender: AnyObject) {
-//        let alert = UIAlertController(title: "Start Over?", message: "Do you really want to start over? There's no turning back from this...", preferredStyle: .Alert)
-//        let OKaction = UIAlertAction(title: "OK", style: .Default, handler: nil)
-//        alert.addAction(OKaction)
-//        presentViewController(alert, animated: true, completion: nil)
-//        reset()
-    }
-    
-    
-    func reset() {
+    func startNewRound() {
         scoreNum = 0
         roundNum = 0
+        currentValue = lroundf(slider.value)
         slider.value = 50
+        targetValue = 1 + Int(arc4random_uniform(100))
+        targetNum.text = "\(targetValue)"
+        score.text = "\(scoreNum)"
+        round.text = "\(roundNum)"
     }
     
-    @IBAction func sliderMoved(slider: UISlider) {
+    @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
         
         print("The value of the slider is now: \(currentValue)")
