05eb039f36ab09b41e55bc8f7e7573604aa381f5
anita <anitay20@gmail.com>
2017-04-05 10:11:25 -0700

    fix: reset map for new location

--- memorablePlaces/memorablePlaces/TableViewController.swift
+++ memorablePlaces/memorablePlaces/TableViewController.swift
@@ -57,6 +57,12 @@ class TableViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "newPlace" {
+            activePlace = -1
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
