5f672cceec737cfad13238dd3dfa4881d05075a3
anita y <anitay20@gmail.com>
2018-10-08 17:21:47 -0700

    Add FlickrPhotosViewController.swift

---
+++ FlickrSearch/FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrSearch
+//
+
+import UIKit
+
+class FlickrPhotosViewController: UIViewController {
+
+    // MARK: - Properties
+    fileprivate let reuseIdentifier = "FlickrCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
