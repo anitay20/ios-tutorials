0cc359cc41f24eec505a2f339e3b2d78254ef8d7
anita y <anitay20@gmail.com>
2019-01-28 22:38:46 -0800

    chore: add Card file

---
+++ itunesU/concentration/concentration/Card.swift
@@ -0,0 +1,23 @@
+//
+//  Card.swift
+//  concentration
+//
+
+import Foundation
+
+struct Card {
+    var isFaceup = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
