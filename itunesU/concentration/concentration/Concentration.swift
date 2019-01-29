a8e802de3b9221fbbcaf9767e93ceafcfb3c3d4f
anita y <anitay20@gmail.com>
2019-01-28 22:39:04 -0800

    chore: add concentration file (business logic)

---
+++ itunesU/concentration/concentration/Concentration.swift
@@ -0,0 +1,55 @@
+//
+//  Concentration.swift
+//  concentration
+//
+
+import Foundation
+
+class Concentration {
+
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceup = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are faced up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceup = false
+                }
+                cards[index].isFaceup = true
+                indexOfOneAndOnlyFaceUpCard = index
+                
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        shuffleCards()
+    }
+    
+    func shuffleCards() {
+        var shuffledCards = [Card]()
+        
+        while !cards.isEmpty{
+            let randomIndex = Int(arc4random_uniform(UInt32(cards.count)))
+            shuffledCards.append(cards[randomIndex])
+            cards.remove(at: randomIndex)
+        }
+        
+        cards = shuffledCards
+    }
+}
