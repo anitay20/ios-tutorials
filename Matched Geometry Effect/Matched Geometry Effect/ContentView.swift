07b58587d33643a14056178f036fcbee4b66d673
anita y <anitay20@gmail.com>
2020-10-08 02:09:40 -0500

    feature: added green circle that expands using matchedGeometryEffect

--- Matched Geometry Effect/Matched Geometry Effect/ContentView.swift
+++ Matched Geometry Effect/Matched Geometry Effect/ContentView.swift
@@ -5,12 +5,42 @@
 //  Created by anita on 10/7/20.
 //
 
+// This is following the tutorial from https://www.appcoda.com/matchedgeometryeffect/
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var expand = false
+    
+    @Namespace private var shapeTransition
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        if expand {
+            
+            // Final State
+            Circle()
+                .fill(Color.green)
+                .matchedGeometryEffect(id: "circle", in: shapeTransition)
+                .frame(width: 300, height: 300)
+                .offset(y: -200)
+                .animation(.default)
+                .onTapGesture {
+                    self.expand.toggle()
+                }
+        } else {
+            
+            // Start State
+            Circle()
+                .fill(Color.green)
+                .frame(width: expand ? 300 : 150, height: expand ? 300 : 150)
+                .offset(y: expand ? -200 : 0)
+                .animation(.default)
+                .onTapGesture {
+                    self.expand.toggle()
+                }
+        }
     }
 }
 
