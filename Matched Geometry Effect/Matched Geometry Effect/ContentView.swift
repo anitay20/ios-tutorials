35986dc9ca30870f3fb31236a7bc2fb8393782b1
anita y <anitay20@gmail.com>
2020-10-07 19:48:49 -0500

    chore: add initial matched geometry effect files

---
+++ Matched Geometry Effect/Matched Geometry Effect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Matched Geometry Effect
+//
+//  Created by anita on 10/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Matched Geometry Effect/Matched Geometry Effect/Matched_Geometry_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matched_Geometry_EffectApp.swift
+//  Matched Geometry Effect
+//
+//  Created by anita on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Matched_Geometry_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
