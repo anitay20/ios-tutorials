676f00c86491157480dd48264d633b663c1e8d85
anita y <anitay20@gmail.com>
2021-01-18 23:02:56 -0600

    chore: finished handling user input tutorial

---
+++ itunesU/LandmarksTutorial/LandmarksTutorial/Views/LandmarksApp.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarksApp.swift
+//  LandmarksTutorial
+//
+//  Created by anita on 1/18/21.
+//  Copyright © 2021 anita. All rights reserved.
+//
+// https://developer.apple.com/tutorials/swiftui/handling-user-input
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
