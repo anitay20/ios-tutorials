d1f1dec027712fe15f0892e1efe2caedec63e7c0
anita y <anitay20@gmail.com>
2021-02-06 02:23:31 -0600

    chore: complete the category view

---
+++ itunesU/LandmarksTutorial/LandmarksTutorial/Views/Categories/CategoryItem.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryItem.swift
+//  LandmarksTutorial
+//
+//  Created by anita on 2/6/21.
+//  Copyright © 2021 anita. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5.0)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
