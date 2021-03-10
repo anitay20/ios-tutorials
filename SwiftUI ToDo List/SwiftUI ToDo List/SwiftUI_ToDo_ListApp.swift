829d661b58f2aeb405dc31837c22882f4f60c887
anita y <anitay20@gmail.com>
2021-03-10 13:56:07 -0600

    feat: completed ToDo List app

---
+++ SwiftUI ToDo List/SwiftUI ToDo List/SwiftUI_ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDo_ListApp.swift
+//  SwiftUI ToDo List
+//
+//  Created by anita on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
