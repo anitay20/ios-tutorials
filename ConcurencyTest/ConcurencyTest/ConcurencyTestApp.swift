550eeaa9b93c049b4d5fe0f8319e2ae04060b4df
anita y <anitay20@gmail.com>
2021-03-23 16:09:23 -0500

    add initial project files

---
+++ ConcurencyTest/ConcurencyTest/ConcurencyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurencyTestApp.swift
+//  ConcurencyTest
+//
+//  Created by anita on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct ConcurencyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
